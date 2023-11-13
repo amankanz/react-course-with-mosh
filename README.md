3c9c60a32f9c4eb973ef51f52607195ddcbbc308
amankanz <akanezamulume.iuea@gmail.com>
2023-11-13 11:18:20 +0300

    react course

---
+++ README.md
@@ -0,0 +1,3 @@
+# react-course-with-mosh
+
+Learning React + TypeScript from Scratch with Mosh as the Instructor!
---
+++ react-app/src/Message.tsx
@@ -0,0 +1,13 @@
+
+// PascalCasing
+function Message() {
+  // JSX: JavaScript XML
+  const name = '';
+  // if name is truthy
+  if (name)
+    return <h1>Hello {name}</h1>;
+  else
+    return <h1>Hello World</h1>;
+}
+
+export default Message;
